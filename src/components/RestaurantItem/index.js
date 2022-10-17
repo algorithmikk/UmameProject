e3f632296048b3b31f335d7b19456b3427031459
Jawad <jawadarifpro@gmail.com>
2022-10-17 00:35:07 -0400

    Implement initial navigation logic

--- src/components/RestaurantItem/index.js
+++ src/components/RestaurantItem/index.js
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import {useNavigation} from "@react-navigation/native";
 
 //restaurant component
 const RestaurantItem = ({ restaurant }) => {
-  {
-    /* the restaurant props is being injected by loading the restaurants.json data
-        this way we can now retrieve all the data using the keys for each restaurant */
+  const navigation = useNavigation();
+  
+  const onPress = () => {
+    navigation.navigate("Restaurant", {id: restaurant.id});
   }
 
   return (
-    <View style={styles.restaurantContainer}>
+    <Pressable onPress={onPress} style={styles.restaurantContainer}>
       <Image
         source={{
           uri: restaurant.image,
@@ -28,7 +30,7 @@ const RestaurantItem = ({ restaurant }) => {
           <Text>{restaurant.rating}</Text>
         </View>
       </View>
-    </View>
+    </Pressable>
   );
 };
 export default RestaurantItem;
